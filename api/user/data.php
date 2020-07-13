4e50b7f14a4a09a80318a1e4306853918fee34c3
Arufu Nagashima <g017a1333@gmail.com>
2020-07-13 15:51:34 +0900

    modified

--- api/user/data.php
+++ api/user/data.php
@@ -7,43 +7,35 @@
 // 以下のコメントを外すと実行時エラーが発生した際にエラー内容が表示される
 // ini_set('display_errors', 'On');
 // ini_set('error_reporting', E_ALL);
-require_once('../Utility.php');
+
+//-------------------------------------------------
+// ライブラリ
+//-------------------------------------------------
+require_once("../util.php");
+require_once("../../model/user.php");
+
 //-------------------------------------------------
 // 引数を受け取る
 //-------------------------------------------------
-// ユーザーIDを受け取る
-$uid = isset($_GET['uid'])?  $_GET['uid']:null;
+$token = UserModel::getTokenfromQuery();
 
-// Validation
-if( ($uid === null) || (!is_numeric($uid)) ){
-  sendResponse(false, 'Invalid uid');
+if( !$token ){
+  sendResponse(false, 'Invalid token');
   exit(1);
 }
-
-//-------------------------------------------------
-// 準備
-//-------------------------------------------------
-
-// 実行したいSQL
-$sql = 'SELECT * FROM User WHERE id=:id';  // Userテーブルの指定列を取得
-
 
 //-------------------------------------------------
 // SQLを実行
 //-------------------------------------------------
 try{
-  $dbh = new PDO($dsn, $user, $pw);   // 接続
-  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  // エラーモード
-  $sth = $dbh->prepare($sql);         // SQL準備
-
-  // プレースホルダに値を入れる
-  $sth->bindValue(':id', $uid, PDO::PARAM_INT);
-
-  // 実行
-  $sth->execute();
-
-  // 実行結果から1レコード取ってくる
-  $buff = $sth->fetch(PDO::FETCH_ASSOC);
+  $user = new UserModel();
+  $uid  = $user->getUserIdByToken($token);
+  if( $uid !== false ){
+    $buff = $user->getRecordById($uid);
+  }
+  else{
+    $buff = false;
+  }
 }
 catch( PDOException $e ) {
   sendResponse(false, 'Database error: '.$e->getMessage());  // 本来エラーメッセージはサーバ内のログへ保存する(悪意のある人間にヒントを与えない)
@@ -55,11 +47,10 @@ catch( PDOException $e ) {
 //-------------------------------------------------
 // データが0件
 if( $buff === false ){
-  sendResponse(false, 'Not Fund user');
+  sendResponse(false, 'Not Found user');
 }
 // データを正常に取得
 else{
   sendResponse(true, $buff);
 }
-
 
